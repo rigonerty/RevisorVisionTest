f3fe644fa4a98bb89fc5d665e99da9aa8b1da1ca
rigonerty <112642804+rigonerty@users.noreply.github.com>
2024-06-19 14:59:50 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './styles/index.css'
+import App from './App.tsx
 import store from "./store"
 import { Provider } from 'react-redux'
 
